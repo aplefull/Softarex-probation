221f76010f871c49ac69ff3071e1ca738bec2724
aplefull <aplefull@gmail.com>
2021-03-09 21:10:45 +0300

    Update Tabs

--- pexels/src/components/Tabs/Tabs.tsx
+++ pexels/src/components/Tabs/Tabs.tsx
@@ -3,15 +3,15 @@ import React from "react";
 import styles from "./Tabs.module.scss";
 
 function Tabs() {
-    return (
-        <div className={styles.tabsWrapper}>
-            <Tab text={'Home'} isSelected={true}/>
-            <Tab text = {'Discover'}  isSelected = {false}/>
-            <Tab text = {'Videos'}  isSelected = {false}/>
-            <Tab text={'Leaderboard'} isSelected={false}/>
-            <Tab text={'Challenges'} isSelected={false}/>
-        </div>
-    );
+  return (
+    <div className={styles.tabsWrapper}>
+      <Tab text={"Home"} isSelected={true} />
+      <Tab text={"Discover"} isSelected={false} />
+      <Tab text={"Videos"} isSelected={false} />
+      <Tab text={"Leaderboard"} isSelected={false} />
+      <Tab text={"Challenges"} isSelected={false} />
+    </div>
+  );
 }
 
 export default Tabs;
