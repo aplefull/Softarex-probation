ef6578c263bac5c18459bfeec06d5e15ddf70e1a
aplefull <aplefull@gmail.com>
2021-03-09 21:12:01 +0300

    Add Photo component

--- pexels/src/components/Container/Photos/Photo/Photo.tsx
+++ pexels/src/components/Container/Photos/Photo/Photo.tsx
@@ -1,16 +1,16 @@
 import React from "react";
-import styles  from "./Photo.module.scss";
+import styles from "./Photo.module.scss";
 
 interface propTypes {
-    photoLink: string;
+  photoLink: string;
 }
 
 function Photo(props: propTypes) {
-    return (
-        <div className={styles.photoWrapper}>
-            <img src={props.photoLink} alt={'p-card'} className={styles.photoImage}/>
-        </div>
-    );
+  return (
+    <div className={styles.photoWrapper}>
+      <img src={props.photoLink} alt={"p-card"} className={styles.photoImage} />
+    </div>
+  );
 }
 
 export default Photo;
