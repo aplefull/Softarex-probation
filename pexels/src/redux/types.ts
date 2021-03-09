d327b69783e21af6d0a08a8e69eb4dddaf66c905
aplefull <aplefull@gmail.com>
2021-03-09 21:22:55 +0300

    Update quotes

--- pexels/src/redux/types.ts
+++ pexels/src/redux/types.ts
@@ -1,4 +1,4 @@
-export const LOAD_PHOTOS = 'LOAD_PHOTOS';
-export const LOAD_HEADER_IMAGE = 'LOAD_HEADER_IMAGE';
-export const HIDE_SEARCH_BAR = 'HIDE_SEARCH_BAR';
-export const SHOW_SEARCH_BAR = 'SHOW_SEARCH_BAR';
+export const LOAD_PHOTOS = "LOAD_PHOTOS";
+export const LOAD_HEADER_IMAGE = "LOAD_HEADER_IMAGE";
+export const HIDE_SEARCH_BAR = "HIDE_SEARCH_BAR";
+export const SHOW_SEARCH_BAR = "SHOW_SEARCH_BAR";
