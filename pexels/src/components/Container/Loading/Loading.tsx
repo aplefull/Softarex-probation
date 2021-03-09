56b143da758f6d9e2875141de576dbeecdeae2e9
aplefull <aplefull@gmail.com>
2021-03-09 21:12:19 +0300

    Add Loading component

--- pexels/src/components/Container/Loading/Loading.tsx
+++ pexels/src/components/Container/Loading/Loading.tsx
@@ -2,18 +2,22 @@ import React from "react";
 import styles from "./Loading.module.scss";
 
 interface propTypes {
-    isHidden: boolean;
+  isHidden: boolean;
 }
 
-function Loading (props: propTypes) {
-    return(
-        <div className={`${styles.loadingWrapper} ${props.isHidden ? styles.hidden : ''}`}>
-            <div className={styles.animationDot}/>
-            <div className={styles.animationDot}/>
-            <div className={styles.animationDot}/>
-            <div className={styles.animationDot}/>
-        </div>
-    );
+function Loading(props: propTypes) {
+  return (
+    <div
+      className={`${styles.loadingWrapper} ${
+        props.isHidden ? styles.hidden : ""
+      }`}
+    >
+      <div className={styles.animationDot} />
+      <div className={styles.animationDot} />
+      <div className={styles.animationDot} />
+      <div className={styles.animationDot} />
+    </div>
+  );
 }
 
 export default Loading;
