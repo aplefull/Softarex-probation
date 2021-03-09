79ab3d37a6ebaf39bb9cd9a208a786cd7bdb7859
aplefull <aplefull@gmail.com>
2021-03-09 21:10:36 +0300

    Update Tab

--- pexels/src/components/Tabs/Tab/Tab.tsx
+++ pexels/src/components/Tabs/Tab/Tab.tsx
@@ -2,19 +2,20 @@ import React from "react";
 import styles from "./Tab.module.scss";
 
 interface TabText {
-    text: string;
-    isSelected: boolean;
+  text: string;
+  isSelected: boolean;
 }
 
 function Tab(props: TabText) {
-    return (
-        <a href="https://aplefull.art" className={`${styles.tab} ${props.isSelected ? styles.active : ''}`}>
-            <p>{props.text}</p>
-            <div className={styles.underline}></div>
-        </a>
-
-    )
-        ;
+  return (
+    <a
+      href="https://aplefull.art"
+      className={`${styles.tab} ${props.isSelected ? styles.active : ""}`}
+    >
+      <p>{props.text}</p>
+      <div className={styles.underline}></div>
+    </a>
+  );
 }
 
 export default Tab;
