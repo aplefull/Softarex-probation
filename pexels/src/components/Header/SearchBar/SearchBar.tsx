127b54e4aabe7e1dd855dcbdb818883532bbb97b
aplefull <aplefull@gmail.com>
2021-03-09 21:10:27 +0300

    Update SearchBar

--- pexels/src/components/Header/SearchBar/SearchBar.tsx
+++ pexels/src/components/Header/SearchBar/SearchBar.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
-import styles from './SearchBar.module.scss';
-import icon from '../../../icons/magnifying-glass.svg';
+import React from "react";
+import styles from "./SearchBar.module.scss";
+import icon from "../../../icons/magnifying-glass.svg";
 
-function SearchBar() {
+interface propTypes {
+  isHidden?: boolean;
+}
+
+function SearchBar(props: propTypes) {
   return (
-    <div className={styles.inputWrapper}>
+    <div
+      className={`${styles.inputWrapper} ${
+        props?.isHidden ? styles.hidden : ""
+      }`}
+    >
       <input type="text" placeholder="Search for free photos" />
       <img src={icon} alt="icon" />
     </div>
   );
-};
+}
 
 export default SearchBar;
