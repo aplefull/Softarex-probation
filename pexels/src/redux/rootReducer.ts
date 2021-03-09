d6ce7e34689a23538aa79971b4578a380baa9935
aplefull <aplefull@gmail.com>
2021-03-09 21:22:12 +0300

    Create reducers

--- pexels/src/redux/rootReducer.ts
+++ pexels/src/redux/rootReducer.ts
@@ -1,8 +1,8 @@
-import {combineReducers} from "redux";
-import {headerReducer} from "./headerReducer";
-import {photosReducer} from "./photosReducer";
+import { combineReducers } from "redux";
+import { headerReducer } from "./headerReducer";
+import { photosReducer } from "./photosReducer";
 
 export const rootReducer = combineReducers({
-    headerReducer: headerReducer,
-    photosReducer: photosReducer
+  headerReducer: headerReducer,
+  photosReducer: photosReducer,
 });
