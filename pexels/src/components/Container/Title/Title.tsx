02d1d8fc84f3105cfd5d54cc5da94a747301d208
aplefull <aplefull@gmail.com>
2021-03-09 21:11:32 +0300

    Update Title

--- pexels/src/components/Container/Title/Title.tsx
+++ pexels/src/components/Container/Title/Title.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import styles from "./Title.module.scss";
 
 function Title() {
-    return (
-        <div className={styles.title}>
-            <p>Free Stock Photos</p>
-            <div className={styles.dropdown}>
-                <p>Trending</p>
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.title}>
+      <p>Free Stock Photos</p>
+      <div className={styles.dropdown}>
+        <p>Trending</p>
+      </div>
+    </div>
+  );
 }
 
 export default Title;
