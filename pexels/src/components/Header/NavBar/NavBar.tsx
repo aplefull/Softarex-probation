9dd0004fad1a998ea99e2093525c461490f3df67
aplefull <aplefull@gmail.com>
2021-03-09 21:10:14 +0300

    Update NavBar

--- pexels/src/components/Header/NavBar/NavBar.tsx
+++ pexels/src/components/Header/NavBar/NavBar.tsx
@@ -1,36 +1,42 @@
-import React from 'react';
-import logo from '../../../images/logo.png';
-import icon from '../../../icons/ellipsis.svg';
-import SearchBar from '../SearchBar/SearchBar';
-import styles from './NavBar.module.scss';
+import React from "react";
+import logo from "../../../images/logo.png";
+import icon from "../../../icons/ellipsis.svg";
+import SearchBar from "../SearchBar/SearchBar";
+import styles from "./NavBar.module.scss";
 
-function NavBar() {
+interface propTypes {
+  isHidden?: boolean;
+}
+
+function NavBar(props: propTypes) {
   return (
-    <div className={styles.navbar}>
-      <a className={styles.navbar__logoWrapper}>
+    <div
+      className={`${styles.navbar} ${props.isHidden ? "" : styles.showColor}`}
+    >
+      <a className={styles.navbar__logoWrapper} href={"/"}>
         <img src={logo} alt="logo" />
         <p>Pexels</p>
       </a>
-      <SearchBar />
+      <SearchBar isHidden={props.isHidden} />
       <div className={styles.navbar__navigationWrapper}>
         <ul className={styles.navigationLinks}>
           <li>
-            <a href="">Explore</a>
+            <a href={"https://www.pexels.com/discover/"}>Explore</a>
           </li>
           <li>
-            <a href="">License</a>
+            <a href={"https://www.pexels.com/license/"}>License</a>
           </li>
           <li>
-            <a href="">Upload</a>
+            <a href={"https://www.pexels.com/join-contributor/"}>Upload</a>
           </li>
           <li>
-            <img src={icon} alt="ellipsis"/>
+            <img src={icon} alt="ellipsis" />
           </li>
         </ul>
         <button className="join-button">Join</button>
       </div>
     </div>
   );
-};
+}
 
 export default NavBar;
