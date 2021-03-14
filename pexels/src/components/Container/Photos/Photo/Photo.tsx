cf9d257da5bcec96baeb34891345aeb3243a4159
aplefull <mraplefull@yandex.by>
2021-03-14 13:07:55 +0300

    Add overlay to the photo component

--- pexels/src/components/Container/Photos/Photo/Photo.tsx
+++ pexels/src/components/Container/Photos/Photo/Photo.tsx
@@ -1,14 +1,36 @@
 import React from 'react';
 import styles from './Photo.module.scss';
+import downloadIcon from '../../../../icons/download.svg';
+import heartIcon from '../../../../icons/heart.svg';
+import addIcon from '../../../../icons/add.svg';
 
 interface propTypes {
   photoLink: string;
+  photographerURL: string;
+  photographerName: string;
+  photoId: string;
 }
 
 function Photo(props: propTypes) {
   return (
     <div className={styles.photoWrapper}>
       <img src={props.photoLink} alt={'p-card'} className={styles.photoImage} />
+      <div className={styles.overlay}>
+        <div className={styles.authorLinkWrapper}>
+          <a href={props.photographerURL}>{props.photographerName}</a>
+        </div>
+        <div className={styles.otherLinksWrapper}>
+          <a href={`https://www.pexels.com/photo/${props.photoId}/download`}>
+            <img src={downloadIcon} alt={'download icon'} />
+          </a>
+          <a href="">
+            <img src={addIcon} alt={'add icon'} />
+          </a>
+          <a href="">
+            <img src={heartIcon} alt={'heart icon'} />
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
