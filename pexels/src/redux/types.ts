ad6d5775baa089a2d5ffc0b36e8cb2200c9710ae
aplefull <mraplefull@yandex.by>
2021-03-14 13:06:48 +0300

    Remove unused types

--- pexels/src/redux/types.ts
+++ pexels/src/redux/types.ts
@@ -1,4 +1,4 @@
 export const LOAD_PHOTOS = 'LOAD_PHOTOS';
 export const LOAD_HEADER_IMAGE = 'LOAD_HEADER_IMAGE';
-export const HIDE_SEARCH_BAR = 'HIDE_SEARCH_BAR';
-export const SHOW_SEARCH_BAR = 'SHOW_SEARCH_BAR';
+export const HIDE_LOADING = 'HIDE_LOADING';
+export const SHOW_LOADING = 'SHOW_LOADING';
