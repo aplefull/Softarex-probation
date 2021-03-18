4547988005534552f61b6fc66fac1e3296b0da01
aplefull <mraplefull@yandex.by>
2021-03-18 15:48:12 +0300

    Remove redundant files

--- pexels/src/index.tsx
+++ pexels/src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { applyMiddleware, compose, createStore } from 'redux';
 import { rootReducer } from './redux/rootReducer';
@@ -25,5 +24,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-reportWebVitals();
