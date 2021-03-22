e709631e19f0ab4db4d198dfffe92bde97d34e35
aplefull <aplefull@gmail.com>
2021-03-23 00:08:49 +0300

    Add Dropdown component

---
+++ pexels/src/components/Container/Photos/Photo/Modal/Dropdown/Dropdown.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import styles from './Dropdown.module.scss';
+import { useDispatch, useSelector } from 'react-redux';
+import { changeSizeOption } from '../../../../../../redux/actions';
+
+interface PropTypes {
+  photo: any;
+}
+
+function Dropdown(props: PropTypes) {
+  let sizeOptions: Array<any> = [];
+  let selectedOption = useSelector(
+    (state: any) => state.dropdownReducer.selectedOption
+  );
+  let dispatch = useDispatch();
+
+  if (props.photo?.src !== undefined) {
+    sizeOptions = Object.entries(props.photo.src).filter((el: Array<any>) => {
+      return ['original', 'large', 'medium', 'small'].some(
+        (val: string) => val === el[0]
+      );
+    });
+  }
+
+  return (
+    <div className={styles.dropdownWrapper}>
+      <p className={styles.text}>Choose a size:</p>
+      <form
+        onChange={(e: any) => {
+          dispatch(changeSizeOption(e.target.getAttribute('data-size')));
+        }}
+      >
+        <ul>
+          {sizeOptions.map((el: any, index: number) => {
+            return (
+              <li key={index}>
+                <label>
+                  <input type={'radio'} name={'size'} data-size={el[0]} />
+                  <p>{el[0]}</p>
+                </label>
+              </li>
+            );
+          })}
+        </ul>
+      </form>
+      <button
+        className={styles.downloadButton}
+        onClick={async () => {
+          const res = await fetch(props.photo.src[selectedOption]);
+          const blob = await res.blob();
+          const blobURL = URL.createObjectURL(blob);
+          const a = document.createElement('a');
+          a.href = blobURL;
+          a.setAttribute(
+            'download',
+            `pexels-${props.photo.photographer.replace(/\s/g, '-')}-${
+              props.photo.id
+            }.jpg`
+          );
+          a.click();
+          URL.revokeObjectURL(blobURL);
+        }}
+      >
+        Free Download
+      </button>
+    </div>
+  );
+}
+
+export default Dropdown;
