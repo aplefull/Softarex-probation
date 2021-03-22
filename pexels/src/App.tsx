7c11ea37cf1c6def856e097f7fd6e1f50f37d8f4
aplefull <aplefull@gmail.com>
2021-03-23 00:16:03 +0300

    Add react router

--- pexels/src/App.tsx
+++ pexels/src/App.tsx
@@ -5,6 +5,8 @@ import Container from './components/Container/Container';
 import Modal from './components/Container/Photos/Photo/Modal/Modal';
 import { useSelector } from 'react-redux';
 import { RootState } from './redux/rootReducer';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import NavBar from "./components/Header/NavBar/NavBar";
 
 function App() {
   const isHidden = useSelector(
@@ -12,12 +14,24 @@ function App() {
   );
 
   return (
-    <>
-      <Header />
-      <Tabs />
-      <Container />
-      <Modal isHidden={isHidden} />
-    </>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={'/'}>
+          <>
+            <Header />
+            <Tabs />
+            <Container />
+            <Modal isHidden={isHidden} />
+          </>
+        </Route>
+        <Route exact path={'/search'}>
+          <>
+            <NavBar isHidden={false}/>
+            <Container/>
+          </>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
