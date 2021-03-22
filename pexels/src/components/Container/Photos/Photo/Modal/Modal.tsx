c5079b2a6baeb65ee915cf577fc44eeeed8c36cb
aplefull <aplefull@gmail.com>
2021-03-23 00:13:50 +0300

    Add working dropdown to modal compoment

--- pexels/src/components/Container/Photos/Photo/Modal/Modal.tsx
+++ pexels/src/components/Container/Photos/Photo/Modal/Modal.tsx
@@ -4,24 +4,23 @@ import styles from './Modal.module.scss';
 import closeIcon from '../../../../../icons/close.svg';
 import heartIcon from '../../../../../icons/heart.svg';
 import addIcon from '../../../../../icons/add.svg';
+import dropdownIcon from '../../../../../icons/dropdown.svg';
 import { hideModal } from '../../../../../redux/actions';
+import Dropdown from './Dropdown/Dropdown';
 
-interface propTypes {
+interface PropTypes {
   isHidden: boolean;
   modalID: number;
   photos: Array<any>;
   hideModal: MouseEventHandler<HTMLDivElement>;
 }
 
-function Modal(props: propTypes) {
+function Modal(props: PropTypes) {
   if (props.isHidden) document.body.style.overflow = '';
   else document.body.style.overflow = 'hidden';
 
-  let openedPhoto: any = props?.photos.filter(
-    (photo: any) => photo.id === props.modalID
-  )[0];
-
-  console.log(openedPhoto);
+  let openedPhoto: any =
+    props?.photos.filter((photo: any) => photo.id === props.modalID)[0] || null;
 
   return (
     <div className={`${styles.overlay} ${props.isHidden ? styles.hidden : ''}`}>
@@ -31,19 +30,30 @@ function Modal(props: propTypes) {
       <div className={styles.contentWrapper}>
         <section className={styles.topBar}>
           <div className={styles.authorSection}>
-            <a href={openedPhoto?.photographer_url}>{openedPhoto?.photographer}</a>
+            <a href={openedPhoto?.photographer_url}>
+              {openedPhoto?.photographer}
+            </a>
             <button className={styles.whiteButton}>Follow</button>
             <button className={styles.whiteButton}>Donate</button>
           </div>
           <div className={styles.actionsSection}>
-            <button className={`${styles.whiteButton} ${styles.btnLarge}`}><img src={heartIcon} alt=""/> Like</button>
-            <button className={`${styles.whiteButton} ${styles.btnLarge}`}><img src={addIcon} alt=""/> Collect</button>
-            <div>
-              <button></button>
-              <div>
-                <div><img src="" alt=""/></div>
-                <div></div>
+            <button className={`${styles.whiteButton} ${styles.btnLarge}`}>
+              <img src={heartIcon} alt="" /> Like
+            </button>
+            <button className={`${styles.whiteButton} ${styles.btnLarge}`}>
+              <img src={addIcon} alt="" /> Collect
+            </button>
+            <div className={styles.downloadButtonWrapper}>
+              <a
+                href={`https://pexels.com/photo/${openedPhoto?.id}/download`}
+                className={styles.downloadButton}
+              >
+                Free Download
+              </a>
+              <div className={styles.dropdownButtonWrapper}>
+                <img src={dropdownIcon} alt="" />
               </div>
+              <Dropdown photo={openedPhoto} />
             </div>
           </div>
         </section>
