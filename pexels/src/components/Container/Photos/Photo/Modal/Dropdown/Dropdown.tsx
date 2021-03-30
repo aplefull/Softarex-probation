b77a1c54ddd46db280d9d687b1552f9ebfcdaf89
aplefull <mraplefull@yandex.by>
2021-03-30 16:03:16 +0300

    Properly type component

--- pexels/src/components/Container/Photos/Photo/Modal/Dropdown/Dropdown.tsx
+++ pexels/src/components/Container/Photos/Photo/Modal/Dropdown/Dropdown.tsx
@@ -1,40 +1,56 @@
-import React from 'react';
+import React, { FormEvent } from 'react';
 import styles from './Dropdown.module.scss';
 import { useDispatch, useSelector } from 'react-redux';
 import { changeSizeOption } from '../../../../../../redux/actions';
+import { PhotoObjectTypes } from '../../../../../../redux/photosReducer';
+import { RootState } from '../../../../../../redux/rootReducer';
 
 interface PropTypes {
-  photo: any;
+  photo: PhotoObjectTypes | null;
+}
+
+function hasKey<O>(obj: O, key: keyof any): key is keyof O {
+  return key in obj;
 }
 
 function Dropdown(props: PropTypes) {
-  let sizeOptions: any = [];
-  let selectedOption = useSelector(
-    (state: any) => state.dropdownReducer.selectedOption
+  let sizeOptions: string[][] = [];
+  let selectedOption: string = useSelector(
+    (state: RootState) => state.dropdownReducer.selectedOption
   );
   let dispatch = useDispatch();
 
   if (props.photo?.src !== undefined) {
-    sizeOptions = Object.entries(props.photo.src).filter((el: any) => {
+    sizeOptions = Object.entries(props.photo.src).filter((el: string[]) => {
       return ['original', 'large', 'medium', 'small'].some(
         (val: string) => val === el[0]
       );
     });
   }
+
   return (
     <div className={styles.dropdownWrapper}>
       <p className={styles.text}>Choose a size:</p>
       <form
-        onChange={(e: any) => {
-          dispatch(changeSizeOption(e.target.getAttribute('data-size')));
+        onChange={(e: FormEvent<HTMLFormElement>) => {
+          const target = e.target as HTMLFormElement;
+          dispatch(changeSizeOption(target.getAttribute('data-size') || ''));
         }}
       >
         <ul>
-          {sizeOptions.map((el: any, index: number) => {
+          {sizeOptions.map((el: string[], index: number) => {
             return (
-              <li key={index} className={`${selectedOption === el[0] ? styles.selected : ''}`}>
+              <li
+                key={index}
+                className={`${selectedOption === el[0] ? styles.selected : ''}`}
+              >
                 <label>
-                  <input type={'radio'} name={'size'} data-size={el[0]} defaultChecked={selectedOption === el[0]}/>
+                  <input
+                    type={'radio'}
+                    name={'size'}
+                    data-size={el[0]}
+                    defaultChecked={selectedOption === el[0]}
+                  />
                   <p>{el[0]}</p>
                 </label>
               </li>
@@ -45,19 +61,25 @@ function Dropdown(props: PropTypes) {
       <button
         className={styles.downloadButton}
         onClick={async () => {
-          const res = await fetch(props.photo.src[selectedOption]);
-          const blob = await res.blob();
-          const blobURL = URL.createObjectURL(blob);
-          const a = document.createElement('a');
-          a.href = blobURL;
-          a.setAttribute(
-            'download',
-            `pexels-${props.photo.photographer.replace(/\s/g, '-')}-${
-              props.photo.id
-            }.jpg`
-          );
-          a.click();
-          URL.revokeObjectURL(blobURL);
+          let fetchURL;
+          if (props.photo?.src && hasKey(props.photo.src, selectedOption)) {
+            fetchURL = props.photo.src[selectedOption];
+          }
+          if (fetchURL !== undefined) {
+            const res = await fetch(fetchURL);
+            const blob = await res.blob();
+            const blobURL = URL.createObjectURL(blob);
+            const a = document.createElement('a');
+            a.href = blobURL;
+            a.setAttribute(
+              'download',
+              `pexels-${props.photo?.photographer.replace(/\s/g, '-')}-${
+                props.photo?.id
+              }.jpg`
+            );
+            a.click();
+            URL.revokeObjectURL(blobURL);
+          }
         }}
       >
         Free Download
